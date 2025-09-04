54d03ad7cebd0caf20644ea21695d90f9016cf24
MuhammadIrfannnnn <mohammedirfanadenwla@gmail.com>
2025-09-04 18:58:16 +0500

    creating constructor destructor setter and getter

--- 24k-0740-lab1/Task3.cpp
+++ 24k-0740-lab1/Task3.cpp
@@ -3,6 +3,20 @@ using namespace std;
 
 class Exam{
     int *marks;
+
+    public:
+    Exam(int m){
+        marks=new int(m);
+    }
+    void set(int m){
+        *marks=m;
+    }
+    int get(){
+        return *marks;
+    }
+    ~Exam(){
+        delete marks;
+    }
 };
 
 int main(){
