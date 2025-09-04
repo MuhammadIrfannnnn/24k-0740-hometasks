afe10835f759b0442a256ff4220bdf8994ecfd01
MuhammadIrfannnnn <mohammedirfanadenwla@gmail.com>
2025-09-04 17:38:01 +0500

    Creating copy constructor and assignment operator

--- Task1.cpp
+++ Task1.cpp
@@ -14,4 +14,23 @@ class ResearchPaper{
             authors[i]=s[i];
         }
     }
+        ResearchPaper(const ResearchPaper& other) {
+        n=other.n;
+        authors=new string[n];
+        for (int i = 0; i<n; i++) {
+            authors[i]=other.authors[i];
+        }
+    }
+
+        ResearchPaper& operator=(const ResearchPaper& other) {
+        if (this != &other) {
+            delete[] authors; 
+            n = other.n;
+            authors=new string[n];
+            for (int i=0;i<n;i++) {
+                authors[i]=other.authors[i];
+            }
+        }
+        return *this;
+    }
 };
