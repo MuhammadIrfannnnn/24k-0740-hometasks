52a98d96d8c6129dfa7dd3cacaaa4d7b3b857f54
MuhammadIrfannnnn <mohammedirfanadenwla@gmail.com>
2025-09-04 17:52:52 +0500

    creating parameterize and copy constructor and copy assignment operator

--- 24k-0740-lab1/Task1.cpp
+++ 24k-0740-lab1/Task1.cpp
@@ -4,4 +4,30 @@ using namespace std;
 class ResearchPaper{
     string *authors;
     int n;
+
+    public:
+        ResearchPaper(int n, string s[]):n(n){
+        authors = new string[n];
+        for (int i=0;i<n;i++) {
+            authors[i]=s[i];
+        }
+    }
+    ResearchPaper(const ResearchPaper& other) {
+        n=other.n;
+        authors=new string[n];
+        for (int i=0;i<n;i++) {
+            authors[i]=other.authors[i];
+        }
+    }
+      ResearchPaper& operator=(const ResearchPaper& other) {
+        if (this != &other) {
+            delete[] authors; 
+            n=other.n;
+            authors = new string[n];
+            for (int i=0;i<n;i++) {
+                authors[i] = other.authors[i];
+            }
+        }
+        return *this;
+    }
 };
