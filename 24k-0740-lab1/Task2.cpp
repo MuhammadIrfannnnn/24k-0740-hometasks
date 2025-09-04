bfbb8c0fb2cfe8440e20297f28225909daade214
MuhammadIrfannnnn <mohammedirfanadenwla@gmail.com>
2025-09-04 18:40:57 +0500

    Creating constructors and destructors

--- 24k-0740-lab1/Task2.cpp
+++ 24k-0740-lab1/Task2.cpp
@@ -3,6 +3,20 @@ using namespace std;
 
 class Box{
     int * vol;
+
+public:
+    Box(){
+        vol=new int(0);
+    }
+    Box(int v){
+        vol=new int(v);
+    }
+    Box(const Box &other){
+        vol=new int(*other.vol);
+    }
+    ~Box(){
+        delete vol;
+    }
 };
 
 int main(){
