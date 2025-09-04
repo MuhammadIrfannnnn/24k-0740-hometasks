a27e87018fc25b5866e5facf61dd4f406fcf7959
MuhammadIrfannnnn <mohammedirfanadenwla@gmail.com>
2025-09-04 18:49:38 +0500

    changing box3 and analyze box2 unchanged (deep copy)

--- 24k-0740-lab1/Task2.cpp
+++ 24k-0740-lab1/Task2.cpp
@@ -33,4 +33,7 @@ int main(){
     cout<<"Box 2 vol: "<<b2.getvol()<<endl;
     Box b3(b2);
     cout<<"Box 3 vol: "<<b3.getvol()<<endl;
+    b3.setvol(100);
+    cout<<"Box 3 new vol: "<<b3.getvol()<<endl;
+    cout<<"Box 2 unchanged vol: "<<b2.getvol()<<endl;
 }
