1dd3a3ee41ed4e1f2a1c62ed1348946133cb2412
MuhammadIrfannnnn <mohammedirfanadenwla@gmail.com>
2025-10-26 00:04:04 +0500

    Add files via upload

---
+++ New folder/Task5.cpp
@@ -0,0 +1,94 @@
+#include<iostream>
+#include<cctype>
+#include<cmath>
+using namespace std;
+
+class Stack{
+public:
+    int top;
+    int*arr;
+    int n;
+
+    Stack(int n):n(n){
+        arr=new int[n];
+        top=-1;
+    }
+    void push(int x){
+        if(top==n-1){
+            cout<<"Stack is overflow!"<<endl;
+            return;
+        }
+        arr[++top]=x;
+    }
+    int pop(){
+        if(top==-1){
+            cout<<"Stack is underflow!"<<endl;
+            return 0;
+        }
+        return arr[top--];
+    }
+    int peek(){
+          if(top==-1){
+            cout<<"Stack is underflow!"<<endl;
+            return 0;
+        }
+        return arr[top];
+    }
+    bool empty(){
+        return top==-1;
+    }
+
+    
+      ~Stack() {
+        delete[] arr;
+    }
+};
+
+int evaluateresult(const string& eq){
+    Stack S(eq.length());
+    for(int i=eq.length()-1;i>=0;i--){
+        if(isdigit(eq[i])){
+            S.push(eq[i]-'0');
+        }
+        else{
+            int val1=S.pop();
+            int val2=S.pop();
+            int result;
+            switch (eq[i])
+            {
+            case '+':
+                result=val1+val2;
+                break;
+            case '-':
+                result=val1-val2;
+                break;
+            case '*':
+                result=val1*val2;
+                break;
+            case '/':
+                if(val2==0){
+                    cout<<"error"<<endl;
+                    return 0;
+                }
+                result=val1/val2;
+                break;
+            case '^':
+                result=pow(val1,val2);
+                break;
+            default:
+                cout<<"Invalid"<<endl;
+                return 0;
+            }
+            S.push(result);
+        }
+    }
+    return S.peek();
+}
+
+int main(){
+    string postfix="-+23*49";
+    int res=evaluateresult(postfix);
+    cout<<"Final result : "<<res<<endl;
+    return 0;
+
+}
